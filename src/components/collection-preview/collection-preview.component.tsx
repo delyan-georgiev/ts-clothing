cad4631cd86f96b3d4c73e29fcdec0787a968e51
Delyan <Georgiev>
2021-10-05 22:31:52 +0300

    Create the clothing app with typescript and hooks

---
+++ src/components/collection-preview/collection-preview.component.tsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+import './collection-preview.styles.scss';
+import { ShopData} from '../../pages/shop/shop.data';
+
+
+const CollectionPreview = (shopData: ShopData): JSX.Element => (
+	<div className='collection-preview'>
+		<h1 className='title'>{shopData.title.toUpperCase()}</h1>
+		<div className='preview'>
+			{
+				shopData.items
+				.filter((item, index) => index < 4)
+				.map(item => (
+					<div key={item.id}>{item.name}</div>
+				))
+			}
+		</div>
+	</div>
+)
+export default CollectionPreview
---
+++ src/pages/shop/shop.component.tsx
@@ -0,0 +1,20 @@
+﻿import React, { useState } from 'react';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+import SHOP_DATA from './shop.data';
+
+
+const ShopPage = () => {
+	const [collections] = useState(SHOP_DATA);
+
+	return (
+		<div className="shop-page">
+			{
+				collections.map((collection) => (
+					<CollectionPreview key={collection.id} {...collection}/>
+				))
+			}
+		</div>
+	);
+}
+export default ShopPage;
