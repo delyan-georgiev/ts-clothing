c7561042c87809775dc24a6ce50298685b25080b
Delyan <Georgiev>
2021-10-06 09:00:34 +0300

    Create the header

--- src/pages/shop/shop.component.tsx
+++ src/pages/shop/shop.component.tsx
@@ -1,11 +1,11 @@
-﻿import React, { useState } from 'react';
+﻿import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 import SHOP_DATA from './shop.data';
 
 
 const ShopPage = () => {
-	const [collections] = useState(SHOP_DATA);
+	const [collections] = React.useState(SHOP_DATA);
 
 	return (
 		<div className="shop-page">
